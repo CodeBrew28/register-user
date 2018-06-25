7bb93544a5b997ae3bc762dba7300e8d95b0ae59
CodeBrew28 <arshiam2@illinois.edu>
2018-06-24 23:00:59 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const { send, json } = require('micro')
+const mongoose = require('mongoose');
+const { Player } = require('schemas')(mongoose);
+
+mongoose.connect(process.env.MONGO_URL)
+mongoose.Promise = global.Promise
+
+module.exports = register = async (req, res) => {
+    const body = await json(req)
+    if (body.email) {
+        const player = new Player({email: body.email });  
+        try {
+            await player.save()
+        } catch (error) {
+            send(res, 400, "Couldn't add the new user");
+        }
+        const data = { message: 'Welcome ' + body.email };
+        send(res, 200, data);
+    }
+    const data = { message: 'Email not provided' };
+    send(res, 400, data);
+    
+}
+
